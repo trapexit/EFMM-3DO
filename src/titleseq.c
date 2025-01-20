9797d21243bd266ce36625014c5dc09e7970aea3
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-20 16:30:05 -0600

    titleseq.c

--- src/titleseq.c
+++ src/titleseq.c
@@ -38,47 +38,47 @@ extern uint32	ccbextra;
 int
 dotitle ()
 {
-	register CCB	*ccb;
-	CelArray	*ca_logo;
+  register CCB	*ccb;
+  CelArray	*ca_logo;
 
-	/*
-	 * Load stupid damn contemptability logo.
-	 */
-	if (!(ca_logo = parse3DO ("$progdir/LaurieProbstMadeMeUseThis")))
-		die ("Laurie's out to lunch.\n");
+  /*
+   * Load stupid damn contemptability logo.
+   */
+  if (!(ca_logo = parse3DO ("$progdir/LaurieProbstMadeMeUseThis")))
+    die ("Laurie's out to lunch.\n");
 
-	ccb = ca_logo->celptrs[0];
-	ccb->ccb_Flags |= CCB_LAST | CCB_NPABS | CCB_LDSIZE |
-			  CCB_LDPRS | CCB_LDPPMP | CCB_YOXY |
-			  CCB_ACW | ccbextra;
-	ccb->ccb_Flags &= ~(CCB_ACCW | CCB_TWD);
+  ccb = ca_logo->celptrs[0];
+  ccb->ccb_Flags |= CCB_LAST | CCB_NPABS | CCB_LDSIZE |
+    CCB_LDPRS | CCB_LDPPMP | CCB_YOXY |
+    CCB_ACW | ccbextra;
+  ccb->ccb_Flags &= ~(CCB_ACCW | CCB_TWD);
 
-	/*
-	 * Position and render logo, and let it languish there for three
-	 * seconds.
-	 */
-	ccb->ccb_XPos = ((wide - REALCELDIM (ccb->ccb_Width)) / 2) << 16;
-	ccb->ccb_YPos = (high - YMARGIN - REALCELDIM (ccb->ccb_Height)) << 16;
+  /*
+   * Position and render logo, and let it languish there for three
+   * seconds.
+   */
+  ccb->ccb_XPos = ((wide - REALCELDIM (ccb->ccb_Width)) / 2) << 16;
+  ccb->ccb_YPos = (high - YMARGIN - REALCELDIM (ccb->ccb_Height)) << 16;
 
-	fadetolevel (rpvis, 0);
-	SetRast (rpvis, 0);
-	DisplayScreen (rpvis->rp_ScreenItem, 0);
-	DrawCels (rpvis->rp_BitmapItem, ccb);
+  fadetolevel (rpvis, 0);
+  SetRast (rpvis, 0);
+  DisplayScreen (rpvis->rp_ScreenItem, 0);
+  DrawCels (rpvis->rp_BitmapItem, ccb);
 
-	fadeup (rpvis, 32);
-	WaitVBL (vblIO, 180);
-	fadetoblank (rpvis, 32);
+  fadeup (rpvis, 32);
+  WaitVBL (vblIO, 180);
+  fadetoblank (rpvis, 32);
 
-	freecelarray (ca_logo);
+  freecelarray (ca_logo);
 
-	/*
-	 * Play CinePak intro.
-	 */
-	if (playcpak ("$progdir/Streams/PublisherLogo"))
-		return (FC_USERABORT);
+  /*
+   * Play CinePak intro.
+   */
+  if (playcpak ("$progdir/Streams/PublisherLogo"))
+    return (FC_USERABORT);
 
-	if (playcpak ("$progdir/Streams/Intro"))
-		return (FC_USERABORT);
+  if (playcpak ("$progdir/Streams/Intro"))
+    return (FC_USERABORT);
 
-	return (FC_NOP);
+  return (FC_NOP);
 }
